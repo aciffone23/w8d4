4b201e3b49efafe1a880edee5acf71b3e42c1532
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-04-27 16:18:38 -0400

    myBind and sum

---
+++ arg_exercise.js
@@ -0,0 +1,81 @@
+// function sum() {
+//     let args = Array.from(arguments);
+//     let total = 0;
+//     args.forEach((ele) => total += ele);
+//     console.log(total);
+// }
+
+function sum(...nums) {
+    let total = 0
+    nums.forEach((ele) => total += ele);
+    console.log(total);
+}
+
+
+// sum(1, 2, 3, 4) // === 10;
+// sum(1, 2, 3, 4, 5)  //=== 15;
+
+// Function.prototype.myBind = function (ctx) {
+//     let bindArgs = Array.from(arguments).slice(1);
+//     let self = this;
+//     return function ()  {
+//         let callArgs = Array.from(arguments);
+//         return self.apply(ctx, bindArgs.concat(callArgs));
+//     };
+    
+// };
+
+Function.prototype.myBind = function (ctx, ...bindArgs) {
+    let self = this;
+    return function (...callArgs) {
+        return self.apply(ctx, bindArgs.concat(callArgs));
+    };
+    
+}
+
+
+
+class Cat {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    says(sound, person) {
+      console.log(`${this.name} says ${sound} to ${person}!`);
+      return true;
+    }
+  }
+  
+  class Dog {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  
+  const markov = new Cat("Markov");
+  const pavlov = new Dog("Pavlov");
+  
+  markov.says("meow", "Ned");
+  // Markov says meow to Ned!
+  // true
+  
+  // bind time args are "meow" and "Kush", no call time args
+  markov.says.myBind(pavlov, "meow", "Kush")();
+  // Pavlov says meow to Kush!
+  // true
+  
+  // no bind time args (other than context), call time args are "meow" and "a tree"
+  markov.says.myBind(pavlov)("meow", "a tree");
+  // Pavlov says meow to a tree!
+  // true
+  
+  // bind time arg is "meow", call time arg is "Markov"
+  markov.says.myBind(pavlov, "meow")("Markov");
+  // Pavlov says meow to Markov!
+  // true
+  
+  // no bind time args (other than context), call time args are "meow" and "me"
+  const notMarkovSays = markov.says.myBind(pavlov);
+  notMarkovSays("meow", "me");
+  // Pavlov says meow to me!
+  // true
